3bf2e0a80efb37840b4e291c119cd567c9279779
Tomislav Dukez <tomdu3@ymail.com>
2025-07-09 14:14:09 +0100

    refactor main to include arguments

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
 import os
+import sys
+
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
+if len(sys.argv) < 2:
+    print('Error: Argument missing: uv run main.py "here goes the prompt"')
+    sys.exit(1)
+
+user_prompt = sys.argv[1]
+messages = [
+    types.Content(
+        role="user", 
+        parts=[
+            types.Part(text=user_prompt)
+            ]
+        ),
+]
 
 client = genai.Client(api_key=api_key)
 
 def main():
     res = client.models.generate_content(
             model='gemini-2.0-flash-001',
-            contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+            contents=messages,
             )
     print(res.text)
-    usage_meta = res.usage_metadata
-    print(f'Prompt tokens: {usage_meta.prompt_token_count}')
-    print(f'Response tokens: {usage_meta.candidates_token_count}')
+    if len(sys.argv) > 2:
+        if sys.argv[2] == '--verbose':
+            
+            usage_meta = res.usage_metadata
+            print(f'User prompt: {user_prompt}')
+            print(f'Prompt tokens: {usage_meta.prompt_token_count}')
+            print(f'Response tokens: {usage_meta.candidates_token_count}')
 if __name__ == "__main__":
     main()
